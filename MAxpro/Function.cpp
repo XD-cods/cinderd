87f48182b1f176852434a5f54d8278734149b921
XD-cods <77207255+XD-cods@users.noreply.github.com>
2023-01-30 12:00:11 +0300

    interface add

--- MAxpro/Function.cpp
+++ MAxpro/Function.cpp
@@ -1,41 +1,68 @@
 #include "Function.h"
 
+
+//------------------------------------------------------------------------------------------------
+
 char** saving(char** MAS,int w,int h)
 {	
 
-
     MAS = new char* [h];
+    int d = w * 0.3;
+
     for(int i=0;i<h;i++)
     {
-    MAS[i] = new char[w];
+        MAS[i] = new char[w];
     }
-    for (int i = 0; i < h; i++)
+
+    for (int i = 1; i < h; i++)
     {
-        for (int b = 0; b < w; b++)
+        for (int b = 1; b < w; b++)
         {
-           MAS[i][b]=0;
+            MAS[i][b] = ' ';
         }
     }
     for (int i = 0; i < w; i++)
     {        
-        MAS[0][i] = '*';
-        MAS[h - 1][i] = '*';
+        MAS[0][i] = 196;
+    MAS[h - 1][i] = 196;
     }
     for (int i = 0; i < h; i++)
     {
-        MAS[i][0] = '*';
-        MAS[i][w - 1] = '*';
+        MAS[i][0] = '|';
+        MAS[i][w - 1] = '|';
     }
-    
+    for (int i = 1; i < h-4; i++)
+    {
+        MAS[i][d] = 124;
+        MAS[i][2 * d] = 124;
+    }
+    for (int i = 1; i < w-1; i++)
+    {
+        MAS[h-4][i] = 196;   
+    }
+    //углы
+    MAS[0][0] = 218;
+    MAS[h-1][0] = 192;
+    MAS[0][w-1] = 191;
+    MAS[h - 1][w-1] = 217;
+    //
+    MAS[h - 4][d] = 193;
+    MAS[h - 4][2*d] = 193;
+    MAS[h - 4][0] = 195;
+    MAS[h - 4][w - 1] = 180;
+    MAS[0][d] = 194;
+    MAS[0][2 * d] = 194;
 
+
+    
+    
 	return MAS;
 }
 
-
+//------------------------------------------------------------------------------------------------
 
 void reading(char** MAS,int w,int h)
 {
-    
     for (int i = 0; i < h; i++)
     {
         for (int b = 0; b < w; b++)
@@ -45,3 +72,5 @@ void reading(char** MAS,int w,int h)
         cout << '\n';
     }
 }
+
+//------------------------------------------------------------------------------------------------
--- MAxpro/MAxpro.cpp
+++ MAxpro/MAxpro.cpp
@@ -5,22 +5,23 @@
 int main()
 {
     char** arr = 0;
-
     int w, h;
-    HANDLE hWndConsole;
-    if (hWndConsole = GetStdHandle(-12))
+    while (1)
     {
-        CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-        if (GetConsoleScreenBufferInfo(hWndConsole, &consoleInfo))
+        HANDLE hWndConsole;
+        if (hWndConsole = GetStdHandle(-12))
         {
-            w = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
-            h = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+            CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+            if (GetConsoleScreenBufferInfo(hWndConsole, &consoleInfo))
+            {
+                w = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+                h = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+            }
         }
+        arr = saving(arr, w, h);
+        reading(arr, w, h);
+        system("pause>nul");
+        system("cls");
     }
-    
-
-    arr = saving(arr, w, h);  
-    reading(arr, w, h);
-    system("pause>nul");
     return 0;
 }
